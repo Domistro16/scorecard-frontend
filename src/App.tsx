1044b4b87ae687d1f0126e6ef9ddf4141ed31baa
Desmond Egwurube <desmondesih@gmail.com>
2025-07-04 09:10:05 +0100

    new message

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,16 @@ import axios from "axios";
 import contractABI from "../contract-abi.json";
 import priceABI from "../price-abi.json";
 import "./App.css";
+import * as fabric from "fabric";
+
+interface TextBox {
+  x: number;
+  y: number;
+  width: number;
+  align: "left" | "center" | "right";
+  size: number;
+  color?: string;
+}
 
 const contractAddress = "0x2B20F646CEdB8D40f2a37358A3b712ced3D5B294";
 interface Metadata {
@@ -40,8 +50,9 @@ const App: React.FC = () => {
   const [src, setImg] = useState<string | null>(null);
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const imgRef = useRef<HTMLImageElement | null>(null);
-  const imageSrc = "/token.jpg"; // Replace with actual path
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const { data, isPending, writeContractAsync } = useWriteContract();
+  const fabricCanvasRef = useRef<fabric.Canvas | null>(null); // New ref for fabric instance
   const account = useAccount();
   const { data: priceData } = useReadContract({
     abi: priceABI,
@@ -59,6 +70,19 @@ const App: React.FC = () => {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setAddress(event.target.value);
   };
+  useEffect(() => {
+    const canvasEl = canvasRef.current;
+    console.log("canvasEl", canvasEl);
+    const gencanvas = new fabric.Canvas(canvasEl as HTMLCanvasElement);
+    console.log("canvas", gencanvas);
+    gencanvas.width = 500;
+    gencanvas.height = 500;
+    fabricCanvasRef.current = gencanvas; // Store in ref
+
+    return () => {
+      gencanvas.dispose();
+    };
+  }, [canvasRef]);
 
   const generateImage = async (
     first: string,
@@ -69,44 +93,136 @@ const App: React.FC = () => {
   ) => {
     const f = address.slice(0, 4);
     const l = address.slice(-4);
-    const canvas = document.createElement("canvas");
-    canvas.width = 900;
-    canvas.height = 500;
-    const ctx = canvas.getContext("2d");
-    console.log(f, l, status);
+    const canvas = fabricCanvasRef.current;
 
-    if (!ctx) return;
+    console.log(first, last, status, count, cat);
+    if (!canvas) return;
+    canvas.clear();
+    const boxes = {
+      holder: {
+        x: 0.37,
+        y: 0.22,
+        width: 0.3,
+        align: "left",
+        size: 18,
+      } as TextBox,
+      tx: {
+        x: 0.68,
+        y: 0.225,
+        width: 0.22,
+        align: "left",
+        size: 14,
+      } as TextBox,
+      first: {
+        x: 0.51,
+        y: 0.443,
+        width: 0.11,
+        align: "left",
+        size: 10,
+      } as TextBox,
+      last: {
+        x: 0.67,
+        y: 0.443,
+        width: 0.11,
+        align: "right",
+        size: 10,
+      } as TextBox,
+      tag: {
+        x: 0.49,
+        y: 0.59,
+        width: 0.3,
+        align: "center",
+        size: 24,
+        color: "#FFD700",
+      } as TextBox,
+      unknown: {
+        x: 0.56,
+        y: 0.788,
+        width: 0.15,
+        align: "center",
+        size: 10,
+        color: "#FFD700",
+      } as TextBox,
+    };
 
-    const image = new Image();
-    image.src = imageSrc; // Use the image source defined above
-    console.log("Image source:", imageSrc);
-    image.crossOrigin = "anonymous";
-    image.onload = () => {
-      ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-      ctx.font = "bold 20px Arial";
-      ctx.fillStyle = "#fff";
-      ctx.rotate(-Math.PI / 14);
-      ctx.fillText(first, 160, 345);
-      ctx.fillText(last, 160, 410);
-      ctx.fillText(count, 560, 480);
-      ctx.fillStyle = "#FFD700";
-      ctx.fillText(`${f}...${l}`, 290, 473);
-      ctx.fillStyle = "#fff";
-      ctx.rotate(-Math.PI / 4);
-      ctx.font = "bold 30px Arial";
-      ctx.fillText(cat, 0, 540);
-      ctx.rotate(Math.PI / 4);
+    const addText = (
+      text: string,
+      box: {
+        x: number;
+        y: number;
+        width: number;
+        align: "left" | "center" | "right";
+        size: number;
+        color?: string;
+      }
+    ) => {
+      const absWidth = box.width * 500;
+      console.log("absWidth", absWidth);
+      console.log(text);
+      console.log(box);
+      const txt = new fabric.Textbox(text.toString(), {
+        fontSize: box.size,
+        fill: box.color || "#fff",
+        fontWeight: "bold",
+        fontFamily: "Arial",
+        textAlign: box.align,
+        width: absWidth,
+        selectable: false,
+      });
 
-      const fishImage = new Image();
-      fishImage.src = `./${status}.png`;
-      fishImage.crossOrigin = "anonymous";
-      fishImage.onload = () => {
-        ctx.drawImage(fishImage, 530, 300, 100, 100);
-        ctx.font = "bold 30px Arial";
-        ctx.fillStyle = "#FFD700";
-        ctx.fillText(status, 540, 290);
-        setImg(canvas.toDataURL("image/png"));
-        canvas.toBlob((blob) => {
+      txt.set({
+        left: box.x * 500,
+        top: box.y * 500,
+      });
+      console.log("txt", txt);
+      canvas.add(txt);
+      canvas.renderAll();
+    };
+
+    const bgImg = new Image();
+    bgImg.crossOrigin = "anonymous";
+    bgImg.src = "/token.jpg";
+    bgImg.onload = () => {
+      console.log(bgImg);
+      const bg = new fabric.FabricImage(bgImg, {
+        originX: "left",
+        originY: "top",
+        scaleX: 500 / bgImg.width,
+        scaleY: 500 / bgImg.height,
+        selectable: false,
+      });
+      console.log(bg);
+      canvas.backgroundImage = bg;
+      canvas.renderAll();
+      console.log(bg);
+      Object.entries(boxes).forEach(([key, box]) => {
+        const textMap: Record<string, string> = {
+          holder: `${f}...${l}`,
+          tx: count,
+          first: first !== 'null' ? first : "NONE",
+          last: last !== 'null' ? last : "NONE",
+          tag: status,
+          unknown: cat,
+        };
+        console.log("key", textMap[key]);
+        addText(textMap[key], box);
+      });
+
+      const fishImgEl = new Image();
+      fishImgEl.crossOrigin = "anonymous";
+      fishImgEl.src = `./${status}.png`;
+      fishImgEl.onload = () => {
+        const fish = new fabric.FabricImage(fishImgEl, {
+          left: 80,
+          top: 200,
+          scaleX: 160 / fishImgEl.width,
+          scaleY: 240 / fishImgEl.height,
+          selectable: false,
+        });
+        canvas.add(fish);
+        canvas.renderAll();
+        setImg(canvas.toDataURL({ format: "jpeg", multiplier: 2 }));
+        canvas.getElement().toBlob((blob) => {
           if (blob) {
             const file = new File([blob], "MySafucard.png", {
               type: "image/png",
@@ -116,7 +232,6 @@ const App: React.FC = () => {
         }, "image/png");
       };
     };
-    console.log(image);
   };
 
   const downloadCanvas = () => {
@@ -290,23 +405,22 @@ const App: React.FC = () => {
     }
   };
   return (
-    <div className="min-h-screen flex flex-col bg-gray-100 dark:bg-gray-900 justify-center items-center p-6">
-      <div className="absolute top-0 right-0 p-4">
+    <div className="min-h-screen flex flex-col bg-gray-100 dark:bg-gray-900 justify-center items-center p-2 md:p-6">
+      <div className="absolute top-0 right-2 md:p-4 pt-2">
         <ConnectButton showBalance={true} />
       </div>
       {status && (
-        <div className="flex-col items-center justify-center w-full max-w-3xl mx-auto p-6 rounded-lg shadow-lg">
-          <h3 className="text-2xl font-bold text-gray-900 dark:text-white">
+        <div className="flex-col items-center justify-center w-full mx-auto ounded-lg shadow-lg mt-5">
+          <h3 className="text-lg md:text-2xl font-bold text-gray-900 dark:text-white">
             Success! Here's your Very Own SafuScorecard
           </h3>
-          <div className="mt-6 bg-white dark:bg-gray-800 shadow-lg rounded-lg h-80 w-160 relative mx-auto">
+          <div className="mt-6 bg-white dark:bg-gray-800 shadow-lg rounded-lg h-72 w-72 md:h-95 md:w-95 relative mx-auto">
             <img
-              ref={imgRef}
               src={src as string}
-              alt="Generated"
-              className="h-80 absolute w-160"
-            ></img>
-            <div className="bg-white dark:bg-gray-900 shadow-lg rounded-lg h-80 w-160 opacity-0 absolute flex space-x-10 justify-center items-center text-white opacity-0 transition-all ease-in-out hover:opacity-50 duration-300 ">
+              ref={imgRef}
+              className="h-72 w-72 md:h-95 md:w-95 absolute"
+            />
+            <div className="bg-white dark:bg-gray-900 shadow-lg rounded-lg h-72 w-72 md:h-95 md:w-95 opacity-0 absolute flex space-x-10 justify-center items-center text-white opacity-0 transition-all ease-in-out hover:opacity-50 duration-300 ">
               <button
                 className="text-5xl cursor-pointer"
                 type="submit"
@@ -323,30 +437,32 @@ const App: React.FC = () => {
             </div>
           </div>
           {!donated ? (
-            <div className="text-center mt-6 text-gray-900 dark:text-gray-300">
+            <div className="text-center mt-6 text-gray-900 dark:text-gray-300 items-center flex flex-col">
               <p>
                 Now go flex your SAFU Card on X... Or mint as an for something
                 more REDACTED
               </p>
-              <button
-                className="mt-6 h-15 font-semibold w-35 p-4 bg-orange-700 rounded-full hover:bg-orange-800 cursor-pointer transition-all "
-                onClick={async () => await mintNFT()}
-                disabled={isPending || account.isDisconnected || loading}
-              >
-                Mint NFT
-              </button>
-              <button
-                className="mt-6 ml-6 bg-blue-600 p-4 rounded-full cursor-pointer hover:bg-blue-800 w-60 mx-auto transition-all duration-300"
-                onClick={reset}
-              >
-               Or Shall we try this again?
-              </button>
+              <div className="flex flex-col lg:flex-row items-center mx-auto">
+                <button
+                  className="mt-6 h-15 font-semibold w-35 p-4 bg-orange-700 rounded-full hover:bg-orange-800 cursor-pointer transition-all "
+                  onClick={async () => await mintNFT()}
+                  disabled={isPending || account.isDisconnected || loading}
+                >
+                  Mint NFT
+                </button>
+                <button
+                  className="mt-6 lg:ml-6 font-semibold bg-blue-600 p-4 rounded-full cursor-pointer hover:bg-blue-800 w-60 transition-all duration-300"
+                  onClick={reset}
+                >
+                  Or Shall we try this again?
+                </button>
+              </div>
             </div>
           ) : (
             <div className="text-center font-semibold mt-6 text-gray-900 dark:text-gray-300 flex flex-col">
               <p className="text-green-400 mt-5">Mint Completed: {data}</p>
               <button
-                className="mt-6 bg-blue-600 p-4 rounded-lg cursor-pointer hover:bg-blue-800 w-60 mx-auto transition-all duration-300"
+                className="mt-6 bg-blue-600 p-4 font-semibold rounded-lg cursor-pointer hover:bg-blue-800 w-60 mx-auto transition-all duration-300"
                 onClick={reset}
               >
                 Shall we try this again?
@@ -357,14 +473,14 @@ const App: React.FC = () => {
       )}
       {!status && (
         <div className="w-screen flex flex-col items-center justify-center">
-          <h1 className="text-5xl font-bold text-gray-900 dark:text-white mb-4 text-center">
+          <h1 className="text-4xl md:text-5xl font-bold text-gray-900 dark:text-white mb-4 text-center">
             SafuToken Scorecard
           </h1>
           <h2 className="text-xl text-gray-700 dark:text-gray-300 mb-6">
             Enter your address to get your special SafuScorecard
           </h2>
           <form
-            className="flex flex-col items-center space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4 lg:w-3/4 mx-auto"
+            className="flex flex-col items-center space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4 w-3/4 mx-auto"
             onSubmit={async (event) => {
               event.preventDefault();
               await calculate();
